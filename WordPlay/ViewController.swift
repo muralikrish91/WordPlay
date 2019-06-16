ef63a7318da6b49707d69d08e90b64c8e5304bf9
Murali Krishnan <murali.govindarajulu@imaginea.com>
2019-06-16 19:14:38 +0530

    Initial Commit

---
+++ WordPlay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordPlay
+//
+//  Created by Murali Krishnan on 16/06/19.
+//  Copyright © 2019 Murali Krishnan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
